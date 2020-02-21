ba0f510eb14eae04fdf199b3b08b3b1beba57ee6
Anashkin Andrey <andexds@gmail.com>
2020-02-21 12:27:46 +0300

    Fix ui

--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,7 @@
 
 import { getLocalStyle, createThemeFromStyles, changeStyleColor } from './styles';
 figma.showUI(__html__);
-figma.ui.resize(360, 472);
+figma.ui.resize(360, 474);
 
 figma.ui.onmessage = msg => {
   figma.ui.postMessage({status: 'update'});
